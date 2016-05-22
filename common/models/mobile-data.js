0f4cd8098f5fe0b0eaf214f86f84ad3219f7dabf
Wim Tobback <wim.tobback@optis.be>
2016-05-22 12:51:51 +0200

    added tone analyzer

--- common/models/mobile-data.js
+++ common/models/mobile-data.js
@@ -1,3 +1,37 @@
-module.exports = function(MobileData) {
+var watson = require('watson-developer-cloud');
+var async = require('async');
 
+var tone_analyzer = watson.tone_analyzer({
+  username: 'a3ddab35-61d4-44ee-be03-4e85debdb0f4',
+  password: 'fzOcKiPtY37v',
+  version: 'v3',
+  version_date: '2016-05-19'
+});
+
+module.exports = function(MobileData) {
+  MobileData.observe('before save', function filterProperties(ctx, next) {
+        if (ctx.instance) {
+          async.waterfall([
+            function(cb) {
+              tone_analyzer.tone({ text: ctx.instance.comment.desc },
+                function(err, tone) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    cb(null, tone);
+                  }
+              });
+            },
+            function (tone, cb) {
+              ctx.instance.comment.tone = tone;
+              cb(null);
+            }
+          ], function (err, result) {
+              if (err) next(err);
+              if (!err) next();
+          });
+        } else {
+          next();
+        }
+      });
 };
